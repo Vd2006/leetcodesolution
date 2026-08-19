752cc643a167c3555fd595b051c1886b134bd5db
Vd2006 <dvidhi009@gmail.com>
2026-08-19 11:06:18 +0530

    Time: 41 ms (81.73%) | Memory: 23.6 MB (21.43%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,7 +1,29 @@
 class Solution:
+
     def shortestPalindrome(self, s: str) -> str:
-        rev = s[::-1]
-        for i in range(len(s)):
-            if s.startswith(rev[i:]):
-                return rev[:i] + s
-        return ""
+        if not s:
+            return ""
+
+        rev_s = s[::-1]
+        temp = s + "#" + rev_s
+        n = len(temp)
+        lps = [0] * n
+        length = 0
+        i = 1
+
+        while i < n:
+            if temp[i] == temp[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+
+        longest_palindromic_prefix_len = lps[-1]
+        suffix_to_add = s[longest_palindromic_prefix_len:]
+
+        return suffix_to_add[::-1] + s
